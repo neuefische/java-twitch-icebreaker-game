6ee93e23dca49b8f9cfab804fd9c2adb4403e3a3
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-07-11 18:42:37 +0200

    Implement Chat

--- backend/src/main/java/de/neuefische/javatwitchicebreakergame/backend/GameWebSocketService.java
+++ backend/src/main/java/de/neuefische/javatwitchicebreakergame/backend/GameWebSocketService.java
@@ -3,11 +3,14 @@ package de.neuefische.javatwitchicebreakergame.backend;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -15,9 +18,11 @@ public class GameWebSocketService extends TextWebSocketHandler {
 
     private final GameService gameService;
     private final ObjectMapper objectMapper;
+    private final Set<WebSocketSession> sessions = new HashSet<>();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
         List<Player> players = gameService.getPlayers();
         String json = objectMapper.writeValueAsString(players);
         session.sendMessage(new TextMessage(json));
@@ -26,6 +31,17 @@ public class GameWebSocketService extends TextWebSocketHandler {
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String payload = message.getPayload();
-        session.sendMessage(new TextMessage("Echo " + payload));
+        sessions.forEach(s -> {
+            try {
+                s.sendMessage(new TextMessage(payload));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
     }
 }
