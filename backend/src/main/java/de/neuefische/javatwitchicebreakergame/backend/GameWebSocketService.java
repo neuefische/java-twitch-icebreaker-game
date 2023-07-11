930965ce441873e7931e5ec1c421de1e43312490
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-07-11 18:38:22 +0200

    Initially send all players to all sessions

--- backend/src/main/java/de/neuefische/javatwitchicebreakergame/backend/GameWebSocketService.java
+++ backend/src/main/java/de/neuefische/javatwitchicebreakergame/backend/GameWebSocketService.java
@@ -1,16 +1,27 @@
 package de.neuefische.javatwitchicebreakergame.backend;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class GameWebSocketService extends TextWebSocketHandler {
 
     private final GameService gameService;
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        List<Player> players = gameService.getPlayers();
+        String json = objectMapper.writeValueAsString(players);
+        session.sendMessage(new TextMessage(json));
+    }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
